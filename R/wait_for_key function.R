e9be7b456fabf5a0e8cc29543ff718ff6b9a5604
Adam Yormark <adamyormark@gmail.com>
2019-11-06 21:36:52 -0500

    remove extra wait_for_key function file

--- R/wait_for_key function.R
+++
@@ -1,7 +0,0 @@
-# Wait for Key before moving on to next line function
-
-wait_for_key <- function(key) {
-  if (readline(prompt = cat("Press ", key, " to continue ", sep = "")) != key) {
-    wait_for_key(key)
-  }
-}
