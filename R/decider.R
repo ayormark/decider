062604dbfa45310a5994ffcb3cc9f129dc2ae303
Adam Yormark <adamyormark@gmail.com>
2019-11-06 21:16:26 -0500

    isolated compare function and substituted with source() in decider()

--- R/decider.R
+++ R/decider.R
@@ -104,31 +104,13 @@ decider <- function(csv_path, csv_task_column_name = Task) {
   # The eisenlikert() function asks user to rank Urgency and Importance from 1-5
   # 5 is the most Urgent or most Important
   # Output is a tibble with two elements, Urgency and Importance, as integers
-  source(paste0(getwd(), "/eiskenlikert.R"))
+  source(paste0(getwd(), "/R/eiskenlikert.R"))
 
 
   # A function to choose which of two options is more important
   # To be used within the quickSort() function
   # Output is either -1 or 1, similar output to greaterThan() from rje
-  compare <- function (choice1, choice2) {
-
-    choice1 <- choice1 %>% as.character()
-    choice2 <- choice2 %>% as.character()
-
-    x <- c(choice1, choice2)
-
-    cat(green("Which should be done first?", "\n"))
-
-    # menu(choices, graphics = FALSE, title = NULL)
-    selection <- menu(x, graphics = F, title = NULL)
-
-    if (selection == 1) {
-      return(-1)
-    } else if (selection == 2) {
-      return(1)
-    }
-
-  }
+  source(paste0(getwd(), "/R/compare.R"))
 
   wait_for_key <- function(key) {
     if (readline(prompt = cat("Press ", key, " to continue ", sep = "")) != key) {
