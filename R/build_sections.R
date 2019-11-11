553f6d699b16249ea4563dfb9ad4811c5c064ddd
Adam Yormark <adamyormark@gmail.com>
2019-11-10 20:50:22 -0500

    completed documentation and commenting

--- R/build_sections.R
+++ R/build_sections.R
@@ -10,9 +10,9 @@
 #' @param project_gid The gid identifier for a specific project in Asana,
 #' @export
 #' @examples
-#' asana_import(
-#' ASANA_ACCESS_TOKEN = "add_your_34_chr_asana_access_token",
-#' project_gid = "123456789101112")
+#' project_gid <- "1234567891011121"
+#' sections <- c("Do", "Delegate", "Schedule")
+#' build_sections(sections, project_gid)
 
 ######### Function ########################################################
 
@@ -20,26 +20,23 @@
 # library(tidyverse)
 # library(dplyr)
 
-build_sections <- function(sections, project_gid = Sys.getenv("ASANA_MYTASKS_PROJECT_ID")) {
+build_sections <-
+  function(sections, project_gid = Sys.getenv("ASANA_MYTASKS_PROJECT_ID")) {
 
+    # Get list of sections that currently exist in the project
     current_sections <-
       asn_sections_find_by_project(project = project_gid) %>%
       filter(name != "(no section)")
 
+    # Convert input sections into a tibble for easier manipulation
     sections <- sections %>% enframe(name = NULL) %>% rename(name = value)
 
+    # Filter out section names that already exist in the project
     new_sections <- full_join(sections, current_sections) %>% filter(is.na(gid))
 
-    for (section in new_sections) {
-
+    # Create the sections in the project
+    for (section in new_sections$name) {
       asn_sections_create_in_project(project = project_gid, name = section)
-
     }
-
   }
-
-project_gid <- "1148823248153567"
-sections <- c("Do", "Delegate", "Schedule", "Ya Ya", "okayokay", "Woodledoodleoodle")
-
-build_sections(sections, project_gid)
 
